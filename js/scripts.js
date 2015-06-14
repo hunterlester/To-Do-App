e5ab5a34807537acc5e257fe64d5b40c8639bb92
hunterlester <guilfordhunter@gmail.com>
2015-06-14 11:29:43 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+var counter = 0;
+var tBody = document.querySelector('tbody');
+
+function addItem () {
+	var form = document.querySelector("form");
+	var listItem = form.listInput.value;
+	
+	var tr = document.createElement("tr");
+	tr.setAttribute('id', 'tr' + counter);
+	tBody.appendChild(tr);
+
+	var td = document.createElement("td");
+	td.textContent = listItem;
+	td.setAttribute('id', 'item' + counter);
+	tr.appendChild(td);
+
+	var td1 = document.createElement("td");
+	td1.innerHTML = '<input type="checkbox" id="complete' + counter + '">';
+	tr.appendChild(td1);
+
+	var td2 = document.createElement("td");
+	td2.innerHTML = '<a href="#" class="button secondary tiny" id="edit' + counter + '"><i class="fi-pencil"></i></a>';
+	tr.appendChild(td2);
+
+	var td3 = document.createElement("td");
+	td3.innerHTML = '<a href="#" class="button alert tiny" id="delete' + counter + '"><i class="fi-minus-circle"></i></a>';
+	tr.appendChild(td3);
+
+	var completeID = document.querySelector('#complete' + counter);
+	completeID.addEventListener("click", function() {
+		var findCompleteID = this.getAttribute('id');
+		var checkClass = td.getAttribute('class');
+		if (checkClass === "green") {
+			td.removeAttribute('class');
+			unComplete(findCompleteID);
+		} else {
+			td.setAttribute('class', 'green');
+			itemComplete(findCompleteID);
+		}
+	});
+
+	var editID = document.querySelector('#edit' + counter);
+	editID.addEventListener("click", function() {
+		var findID = this.getAttribute('id');
+		editItem(findID);
+	});
+
+	var deleteID = document.querySelector('#delete' + counter);
+	deleteID.addEventListener("click", function() {
+		var answer = confirm("Are you sure you want to delete this item?");
+		if (answer) {
+			var findTrID = this.getAttribute('id');
+			deleteItem(findTrID);
+		} else {
+			return false;
+		}
+	});
+
+	form.reset();
+	counter++;
+};
+
+function itemComplete(ID) {
+	var numID = Number(ID.slice(-1));
+	var getEdit = document.querySelector('#edit' + numID);
+	getEdit.setAttribute('class', 'delete');
+};
+
+function unComplete(ID) {
+	var numID = Number(ID.slice(-1));
+	var getEdit = document.querySelector('#edit' + numID);
+	getEdit.setAttribute('class', 'button secondary tiny');
+};
+
+function editItem(findID) {
+	var numID = Number(findID.slice(-1));
+
+	var getTD = document.querySelector('#item' + numID);
+	getTD.innerHTML = '<input type="text" id="newInput'+ numID +'"><a href="#" class="button" id="newSubmit'+ numID +'">Save</a>';
+
+	var submitID = document.querySelector('#newSubmit' + numID);
+	submitID.addEventListener("click", function() {
+		var newInputID = document.querySelector('#newInput' + numID).value;
+		getTD.innerHTML = newInputID;
+	});
+};
+
+function deleteItem(findTrID) {
+	var numID = Number(findTrID.slice(-1));
+	var getTR = document.querySelector('#tr' + numID);
+	tBody.removeChild(getTR);
+}
